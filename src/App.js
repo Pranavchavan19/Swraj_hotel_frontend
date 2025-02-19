c1e99aa85328982b60621e87a65647956541cada
Pranavbc <pranav1952000@gmail.com>
2025-02-19 11:35:50 +0530

    profiile page add

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import HomePage from './component/home/HomePage';
 import AllRoomsPage from './component/booking_rooms/AllRoomsPage';
 import FindBookingPage from './component/booking_rooms/FindBookingPage';
 import RoomDetailsPage from './component/booking_rooms/RoomDetailsPage';
+import LoginPage from './component/auth/LoginPage';
+import RegisterPage from './component/auth/RegisterPage';
 
 function App() {
   return (
@@ -21,6 +23,8 @@ function App() {
                      <Route exact path='/rooms' element={<AllRoomsPage/>}/>
                      <Route  path='/find-booking' element={<FindBookingPage/>}/>
                      <Route  path='/room-details-book/:roomId' element={<RoomDetailsPage/>}/>
+                     <Route  path='/login' element={<LoginPage/>}/>
+                     <Route  path='/register' element={<RegisterPage/>}/>
 
                   </Routes>
                   
